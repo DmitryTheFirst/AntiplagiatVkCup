6d7b3a8500a28eba05cc28a134feaa054616dd87
Smirnyaga <smirnyaga4@yandex.ru>
2015-04-25 01:22:08 +0300

    Initial

---
+++ Antiplagiat/Antiplagiat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antiplagiat
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+
+
+	static class IO
+	{
+		static public String[] ReadFile(String path)
+		{
+			StreamReader sr = new StreamReader(path);
+			return null;
+		}
+	}
+}
