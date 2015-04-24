ea6524bd272a1ae807769a6174401ac026d4712c
Smirnyaga <smirnyaga4@yandex.ru>
2015-04-25 02:17:43 +0300

    Main

--- Antiplagiat/Antiplagiat/xakkep9000.cs
+++ Antiplagiat/Antiplagiat/xakkep9000.cs
@@ -6,58 +6,58 @@ using System.Threading.Tasks;
 
 namespace Antiplagiat
 {
-    public enum Language
-    {
-        CPLUSPLUS,
-        C,
-        JAVA,
-        CSHARP,
-        PYTHON,
-        PASCAL,
-        UNKNOWN
-    }
+	public enum Language
+	{
+		CPLUSPLUS,
+		C,
+		JAVA,
+		CSHARP,
+		PYTHON,
+		PASCAL,
+		UNKNOWN
+	}
 
-    public class SourceFile
-    {
-        private Language lagnguage { set; get; }
-        private string content { set; get; }
+	public class SourceFile
+	{
+		private Language lagnguage { set; get; }
+		private string content { set; get; }
 
-        public SourceFile() { }
+		public SourceFile() { }
 
-        public SourceFile(Language language, string content)
-        {
-            this.lagnguage = language;
-            this.content = content;
-        }
+		public SourceFile(Language language, string content)
+		{
+			this.lagnguage = language;
+			this.content = content;
+		}
 
-        //rm cpp c cs java
-        private void RemoveShitCPLUSPLUS()
-        {
+		//rm cpp c cs java
+		private void RemoveShitCPLUSPLUS()
+		{
 
-        }
+		}
 
-        public void removeShit()
-        {
-            switch (lagnguage)
-            {
-                case Language.CPLUSPLUS:
-                    RemoveShitCPLUSPLUS();
-                    break;
-                case Language.C:
-                    break;
-                case Language.JAVA:
-                    break;
-                case Language.CSHARP:
-                    break;
-                case Language.PYTHON:
-                    break;
-                case Language.PASCAL:
-                    break;
-                case Language.UNKNOWN:
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
+		public void removeShit()
+		{
+			switch (lagnguage)
+			{
+				case Language.CPLUSPLUS:
+					RemoveShitCPLUSPLUS();
+					break;
+				case Language.C:
+					break;
+				case Language.JAVA:
+					break;
+				case Language.CSHARP:
+					break;
+				case Language.PYTHON:
+					break;
+				case Language.PASCAL:
+					break;
+				case Language.UNKNOWN:
+					break;
+				default:
+					break;
+			}
+		}
+	}
 }
